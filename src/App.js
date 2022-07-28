7e2f434a601f6e494c655fa6886994c77ec009e0
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-07-28 12:50:35 +0100

    created Navbar component,home and exercisedetails pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import {Routes, Route} from "react-router-dom";
 import { Box } from "@mui/material";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import ExerciseDetail from "./Pages/ExerciseDetail";
 import './App.css';
 
 function App() {
