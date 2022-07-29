673173221b1f5ee0ab478586855c188c6d2f9272
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-07-29 12:14:01 +0100

    build search field using mui TextField

--- src/Components/ExercisesSearch.jsx
+++ src/Components/ExercisesSearch.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { borderRadius, color, textAlign } from "@mui/system";
 
 const ExercisesSearch = () => {
+  
   return (
-    <div>ExercisesSearch</div>
-  )
-}
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        mb="50px"
+        textAlign="center"
+      >
+        Check Out These Awesome Exercises
+      </Typography>
+      <Box position="relative" mb="72px">
+        <TextField
+          sx={{
+            width: { lg: "1000px", xs: "350px",borderRadius:"40px" },
+            backgroundColor:"fff",
+            input: {
+              fontWeight: "700px",
+              color: "#008000",
+              fontSize: "20px",
+              textAlign: "center",
+            },
+            
+          }}
+          height="76px"
+          value=""
+          onChange={(e) => {}}
+          placeholder="Search Exercises"
+          type="text"
+          
+        />
+        <Button className="search-btn" variant="contained" color="success" sx={{ height: "60px", width: {lg:"175px", xs:"80px"}, textTransform:"none", fontSize:{lg:"20px",xs:"15px"}, position:"absolute",right:"0"}}>Search</Button>
+      </Box>
+    </Stack>
+  );
+};
 
-export default ExercisesSearch
+export default ExercisesSearch;
--- src/Components/HeroBanner.jsx
+++ src/Components/HeroBanner.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import banner from "../assets/images/banner-1.png"
-import { NavLink } from "react-router-dom";
-import { Box,Stack, Typography,Button } from "@mui/material"
+import { Box, Typography,Button } from "@mui/material"
 const HeroBanner = () => {
   return (
     <Box sx={{mt: {lg: "212px", xs: "70px"},ml: {sm: "50px"}}} position="relative" p="20px">
@@ -14,7 +13,7 @@ const HeroBanner = () => {
       <Typography  fontSize="25px" lineHeight="35px" mt={2} mb={3} >
         Checkout the most effective exercises
       </Typography>
-      <Button sx={{padding:"10px"}}  variant="contained" color="success" href="#exercises">Explore Exercises</Button>
+      <Button   variant="contained" color="success" href="#exercises">Explore Exercises</Button>
       <Typography fontWeight={600} fontSize={300} color="#008000" sx={{
         opacity:0.1,
         display: {lg: "block", xs: "none"}
