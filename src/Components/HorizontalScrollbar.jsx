49c3b29362127dd4065d383aa37a1e6d1604bb34
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-07-30 11:26:06 +0100

    add scrow lift and right arrows

--- src/Components/HorizontalScrollbar.jsx
+++ src/Components/HorizontalScrollbar.jsx
@@ -1,14 +1,35 @@
-import { Box } from "@mui/material"
-
+import { Box,Typography } from "@mui/material"
+import React, { useContext } from 'react';
 import BodyParts from "./BodyParts"
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu"
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
 
 
 const HorizontalScrollbar = ({data, bodyPart, setBodyPart}) => {
   
   return (
-    <ScrollMenu >
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
       {data.map((item,index) => (
         <Box 
         key={item.id || item}
