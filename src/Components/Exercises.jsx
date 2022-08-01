983a1c23dde8e9bca1810a3439633813e07ded16
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-01 10:55:47 +0100

    implemented pagination

--- src/Components/Exercises.jsx
+++ src/Components/Exercises.jsx
@@ -1,33 +1,54 @@
-import React, {useEffect, useState} from 'react';
-import { Pagination } from "@mui/material/Pagination";
-import { Box, Button, Stack,Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Pagination } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 
-import {excerciseOptions, fetchData} from "../utils/FetchData"
+import { excerciseOptions, fetchData } from "../utils/FetchData";
 import ExerciseCard from "./ExerciseCard";
 
+const Exercises = ({ bodyPart, exercises, setExercises }) => {
+const [currentPage, setCurrentpage] = useState(1)
+const exercisesPerPage = 9;
 
+const indexOfLastExercise =  currentPage * exercisesPerPage
+const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
+const currentExercise = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
 
-const Exercises = ({bodyPart,exercises,setExercises}) => {
-  console.log(exercises);
+  const paginate = (e, value) => {
+    setCurrentpage(value);
+
+    window.scrollTo({top: 1800, behavior:"smooth"})
+  }
+  
   return (
-    <Box id="exercises"
-    sx={{mt:{lg: "110px"}}}
-    mt="50px"
-    p="20px"
-    >
-<Typography variant="h3" mb="46px">
-  Showing Results
-  </Typography>
-  <Stack direction="row" sx={{gap:{lg:"110px", xs:"50px"}}}
-  flexWrap="wrap" justifyContent="center"
-  >
-    {exercises.map((exercise, index) => (
-      <ExerciseCard key={index} exercise={exercise}/> 
-    )
-    )}
-  </Stack>
-    </Box>
-  )
-}
+    <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {currentExercise.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
 
-export default Exercises
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 10 && (
+          <Pagination
+            color="primary"
+            shape="rounded"
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
+      </Stack>
+    </Box>
+  );
+};
+
+export default Exercises;
