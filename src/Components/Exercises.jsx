ba7f1e0c674b888ba3a8035fa6ca3df1ad1726b9
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-08-01 10:26:44 +0100

    map through exercises and pass them to exercisecard component

--- src/Components/Exercises.jsx
+++ src/Components/Exercises.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import { Pagination } from "@mui/material/Pagination";
+import { Box, Button, Stack,Typography } from "@mui/material";
 
-const Exercises = () => {
+import {excerciseOptions, fetchData} from "../utils/FetchData"
+import ExerciseCard from "./ExerciseCard";
+
+
+
+const Exercises = ({bodyPart,exercises,setExercises}) => {
+  console.log(exercises);
   return (
-    <div>Exercises</div>
+    <Box id="exercises"
+    sx={{mt:{lg: "110px"}}}
+    mt="50px"
+    p="20px"
+    >
+<Typography variant="h3" mb="46px">
+  Showing Results
+  </Typography>
+  <Stack direction="row" sx={{gap:{lg:"110px", xs:"50px"}}}
+  flexWrap="wrap" justifyContent="center"
+  >
+    {exercises.map((exercise, index) => (
+      <ExerciseCard key={index} exercise={exercise}/> 
+    )
+    )}
+  </Stack>
+    </Box>
   )
 }
 
